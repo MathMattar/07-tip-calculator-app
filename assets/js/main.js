33f4a03cf23ba09061e64c36ca3a099c6ff8d8b3
MathMattar <mathmattar@hotmail.com>
2023-01-27 15:13:15 -0300

    Update JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,27 @@
 const form = document.getElementById("calculator");
-let bill = document.getElementById("bill");
-let tip = document.querySelectorAll(".tip__btn").value;
-let tipCustom = document.getElementById("btn-six").value;
-let people = document.getElementById("people");
+const bill = document.getElementById("bill");
+const buttons = document.querySelectorAll(".tip__button");
+const people = document.getElementById("people");
+const reset = document.getElementById("reset");
 
-form.addEventListener("keyup", (e) => {
-  e.preventDefault();
+let buttonCustom = document.getElementById("btn-six");
+let resultTip = document.getElementById("result-tip");
+let resultTotal = document.getElementById("result-total");
 
-  checkBill();
-  checkPeople();
-});
-
-function checkBill() {
-  const billValue = bill.value;
-
-  if (billValue === "") {
+//Verificar arquivos em branco, exibir e ocultar alertas
+function checkData() {
+  if (bill.value === "") {
     alert("alert--bill", "Can't be zero");
-    bill.classList.add("alert__border");
-  }
-}
-
-function checkPeople() {
-  const peopleValue = people.value;
-
-  if (peopleValue === "") {
-    alert("alert--people", "Can't be zero");
-    people.classList.add("alert__border")
+    border(bill);
   } else {
-    alert("alert--people", "")
-    people.classList.remove("alert__border")
+    removeAlert("alert--bill", bill);
+  }
+
+  if (people.value === "") {
+    alert("alert--people", "Can't be zero");
+    border(people);
+  } else {
+    removeAlert("alert--people", people);
   }
 }
 
@@ -37,15 +30,59 @@ function alert(element, message) {
   errorAlert.innerHTML = message;
 }
 
-function errorBorder(element) {
-  let input = document.getElementById(element);
+function border(element) {
+  let input = element;
+  input.classList.add("alert__border");
 }
 
-// function calculator (value, percentage, number){
-//     let value = bill.value;
-//     let percentage = tip.value;
-//     let number = people.value;
+function removeAlert(alert, element) {
+  input = element;
+  document.getElementById(alert).innerHTML = "";
+  element.classList.remove("alert__border");
+}
 
-//     resultTip = value * (percentage / 100)/ number;
-//     resultTotal = resultTip + value;
-// }
+form.addEventListener("keyup", (e) => {
+  e.preventDefault();
+
+  checkData();
+});
+
+//Capturar valores: Bill, Tip Amount e Number of People
+
+bill.addEventListener("input", (e) => {
+  e.preventDefault();
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+buttonCustom.addEventListener("input", (e) => {
+  e.preventDefault();
+  console.log(buttonCustom.value);
+});
+
+people.addEventListener("input", (e) => {
+  e.preventDefault();
+});
+
+//Equação
+
+let billValue = bill.value;
+let tipButton = document.querySelector(".tip__button.active");
+let tipCustom = buttonCustom.value;
+let numberOfPeople = people.value;
+
+console.log(tipCustom)
+
+//Imprimir resultado
+
+//Reset
+reset.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  location.reload();
+});
